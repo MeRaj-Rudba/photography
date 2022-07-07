06a4576b7b0977af680a6c6ee6aaadf51ed2d750
unknown <mrudba@gmail.com>
2022-07-07 12:55:14 +0600

    added necessary login and session and other feature

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,30 +1,54 @@
 import axios from "axios";
+import { useSession, getSession, signOut } from "next-auth/react";
 
 export default function Home(props) {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        <h1>Profile Found with {props.user.name}</h1>
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
-    <div>
-      <h1>Profile Page </h1>
-    </div>
+    <>
+      <h1>Profile should not load</h1>
+    </>
   );
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
   const host = process.env.NEXT_PUBLIC_API_HOST;
   // const host = environment.api_url;
-  try {
-    const res = await axios.get(`${host}/get-all`);
 
+  if (session) {
+    try {
+      const res = await axios.get(`${host}/user/${session.user.email}`);
+
+      return {
+        props: {
+          user: res.data.user,
+          session: session,
+        },
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        redirect: {
+          destination: "/sign-in",
+          permanent: false,
+        },
+      };
+    }
+  } else {
     return {
-      props: {
-        photographs: res.data.filter((photo) => photo.isFeatured),
+      redirect: {
+        destination: "/sign-in",
+        permanent: false,
       },
     };
-  } catch (error) {
-    console.log(error);
   }
-  return {
-    props: {
-      data: null,
-    },
-  };
 };
