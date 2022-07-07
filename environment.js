da9b1a21d8e10136b81e6d8169ff166feea21607
unknown <mrudba@gmail.com>
2022-07-07 09:14:27 +0600

    env

--- environment.js
+++ environment.js
@@ -1,4 +1,4 @@
-export default {
-  api_url: "http://localhost:8080",
-  cloudinary_url: "https://api.cloudinary.com/v1_1/rudba/image/upload",
-};
+
+NEXT_PUBLIC_API_HOST=http://localhost:8080
+
+NEXT_PUBLIC_CLOUDINARY=https://api.cloudinary.com/v1_1/rudba/image/upload
