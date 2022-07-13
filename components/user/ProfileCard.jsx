be248150ac028cfb960efb563d75083d9a210cec
unknown <mrudba@gmail.com>
2022-07-13 16:39:46 +0600

    fixed a minor issue

--- components/user/ProfileCard.jsx
+++ components/user/ProfileCard.jsx
@@ -51,22 +51,24 @@ export default function ProfileCard(props) {
               <div className="text-center mt-8 cursor-pointer font-medium  text-transparent bg-clip-text bg-gradient-to-r from-pink-700 to-orange-400 ">
                 {props.user.email}
               </div>
-              <div className="w-full flex place-content-center mt-8 mx-auto ">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 cursor-pointer text-orange-600 hover:text-pink-700"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  onClick={() => props.signOut()}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                  />
-                </svg>
+              <div className="w-full flex place-content-center mt-8 mx-auto  ">
+                <div className="rounded-full bg-zinc-300 dark:bg-zinc-900 p-2 hover:text-pink-700">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 cursor-pointer  hover:text-pink-700"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    onClick={() => props.signOut()}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
           </div>
